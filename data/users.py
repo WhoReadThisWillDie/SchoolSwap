ae957978e3b3aad8ed9db81fc3236103d92f37e4
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-04-24 20:05:55 +0300

    Update users.py

--- data/users.py
+++ data/users.py
@@ -1,25 +1,31 @@
+import datetime
 import sqlalchemy
-from db_session import SqlAlchemyBase
-from sqlalchemy import orm
 from flask_login import UserMixin
+from sqlalchemy import orm
+from sqlalchemy_serializer import SerializerMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from .db_session import SqlAlchemyBase
 
-class User(SqlAlchemyBase, UserMixin):
+class User(SqlAlchemyBase, UserMixin, SerializerMixin):
     __tablename__ = 'users'
-
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
-    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    username = sqlalchemy.Column(sqlalchemy.String, nullable=True, unique=True)
-    classname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
-
-    def __repr__(self):
-        return f'<User> {self.id} {self.name} {self.email}'
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+    phone = sqlalchemy.Column(sqlalchemy.Integer, nullable=True, unique=True)
+    rating = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    goods = orm.relation("Goods", back_populates='user')
 
     def set_password(self, password):
         self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.hashed_password, password)
+
+    def __repr__(self):
+        return f"USER - {self.name}, {self.about}, {self.email}"

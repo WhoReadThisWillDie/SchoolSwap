a8e4ce65af28e4a1423aad5bf5e1169626aac004
p0tato33 <67389389+p0tato33@users.noreply.github.com>
2022-04-24 20:04:50 +0300

    Create category.py

---
+++ data/category.py
@@ -0,0 +1,23 @@
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+association_table = sqlalchemy.Table(
+    'association',
+    SqlAlchemyBase.metadata,
+    sqlalchemy.Column('goods', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('goods.id')),
+    sqlalchemy.Column('category', sqlalchemy.Integer,
+                      sqlalchemy.ForeignKey('category.id'))
+)
+
+
+class Category(SqlAlchemyBase):
+    __tablename__ = 'category'
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True,
+                           autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+
+
+    def __repr__(self):
+        return f"Category ({self.id}, {self.name})"
